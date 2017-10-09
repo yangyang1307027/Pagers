431d19babba968e181d929738c8fb0f16c7ff134
yangyang <614689428@qq.com>
2017-10-09 10:41:31 +0800

    fgfhfhfhgffhgg

--- app/src/main/java/cn/deng/com/pagers/Count.java
+++ app/src/main/java/cn/deng/com/pagers/Count.java
@@ -9,5 +9,6 @@ public class Count  {
     public  static final  String  VIP_TYPE="vip_type";
     public  static final  String BASDHAHDSHAS_SAJDJABDS="BSADA";
     public  static final  String BASDHAHDSHAS_SAJDJABDSs="BSADA";
+    public  static final  String BASDHAHDSHAS_SAJDJABDSss="BSADA";
 
 }
